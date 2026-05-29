ff3bb3ae6880978ea62970d2181b5f8f8a47ff75
Divyanshagrawa01 <95089199+Divyanshagrawa01@users.noreply.github.com>
2026-05-29 19:31:49 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello CI-CD created success!!!");
-         System.out.println("Hello CI-CD created success!!!");
-          System.out.println("Hello CI-CD created success!!!");
-    }
-}
